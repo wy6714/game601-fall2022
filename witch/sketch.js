ea5850619ee886805fbcf8a3d5a6f77f122a53bd
wy6714 <78013025+wy6714@users.noreply.github.com>
2022-11-17 23:37:19 -0500

    ready to work on final

--- witch/sketch.js
+++ witch/sketch.js
@@ -1,18 +1,14 @@
-let witch;
+
 function preload(){
-  witch = loadImage ('image/witch.png');
+
 }
 function setup() {
-  createCanvas(1040, 700);
+  createCanvas(windowWidth,windowHeight);
   
 }
 
 function draw() {
-  background(220);
-  fill(66, 135, 245);
-  strokeWeight(5);
-  ellipse(20,20,20,20);
-  circle(50,50,10);
-  image(witch,100,100);
+  background("#eeeee4");
+ 
 
 }
