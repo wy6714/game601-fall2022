82343ef7325816e2fd2ba2f43a59206bc5546e11
wy6714 <78013025+wy6714@users.noreply.github.com>
2022-11-20 21:07:53 -0500

    try p5.play.js

---
+++ witch/sketch.js
@@ -0,0 +1,18 @@
+let witchSprite;//hold spriteSheet image+info
+let witchAni;//hold animation
+
+function preload(){
+  witchSprite = loadSpriteSheet('assets/B_witch_idle.png', 200,200,6);
+  witchAni = loadAnimation(witchSprite);
+}
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw() {
+	clear();
+  Animation(witchAni,500,400);
+	
+}
